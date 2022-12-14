c36a23e09f69c0a6fa3a78e68c3bb96de98f8dd6
italijancic <italijancic@outlook.com>
2022-12-14 20:50:14 -0300

    Delete unusseles middlewares

--- src/middlewares/products.middlewares.js
+++
@@ -1,8 +0,0 @@
-import { ProductManager } from '../services/products.services.js'
-
-const productManager = new ProductManager('./src/store/products.json')
-
-export const loadProductManager = (req, res, next) => {
-  req.productManager = productManager
-  next()
-}
