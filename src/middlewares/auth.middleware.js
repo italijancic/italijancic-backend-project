f05ccaa5d7f054495b8b8505c2a1437c05799c5f
italijancic <italijancic@outlook.com>
2023-02-23 21:51:37 -0300

    Add /api/sessions/current route

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -15,3 +15,19 @@ export const authMiddleware = (req, res, next) => {
     })
   }
 }
+
+export const checkSession = (req, res, next) => {
+  try {
+    if (req.session.logged) {
+      req.session.touch()
+      next()
+    } else {
+      throw new Error('User not logged')
+    }
+  } catch (error) {
+    res.status(403).json({
+      success: STATUS.FAIL,
+      message: error.message
+    })
+  }
+}
